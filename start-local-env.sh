2df9261818b4cb88579923021f2024188e4a6749
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2026-01-09 16:03:51 +0200

    LAMPI-358: Add initial start-local-env.sh

---
+++ start-local-env.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+CURRENT_DIR="$(dirname "$(readlink -f "$0")")"
+
+function stop() {
+  pushd "${CURRENT_DIR}"
+  docker compose -f ./docker-compose.yml down
+  popd
+}
+
+function main() {
+  pushd "${CURRENT_DIR}"
+  docker compose -f ./docker-compose.yml up
+  popd
+}
+
+main "$@"
