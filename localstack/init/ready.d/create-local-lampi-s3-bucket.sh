80127d94abaaa81c6f4aedb1b86e8542a0b9e69f
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2026-01-09 16:23:56 +0200

    LAMPI-358: Add files to S3 during startup

---
+++ localstack/init/ready.d/create-local-lampi-s3-bucket.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+CURRENT_DIR="$(dirname "$(readlink -f "$0")")"
+
+function create_bucket() {
+  awslocal s3 mb s3://oph-lampi-local
+}
+
+function copy_files_to_bucket() {
+  awslocal s3 cp "${CURRENT_DIR}"/data/koodi.csv s3://oph-lampi-local/koodi.csv
+  awslocal s3 cp "${CURRENT_DIR}"/data/koodisto.schema s3://oph-lampi-local/koodisto.schema
+  awslocal s3 cp "${CURRENT_DIR}"/data/manifest.json s3://oph-lampi-local/manifest.json
+  awslocal s3 cp "${CURRENT_DIR}"/data/relaatio.csv s3://oph-lampi-local/relaatio.csv
+}
+
+function list_files() {
+  aws --endpoint-url=http://127.0.0.1:4566 s3 ls --recursive s3://oph-lampi-local/
+}
+
+function main() {
+  create_bucket
+  copy_files_to_bucket
+}
+
+main
