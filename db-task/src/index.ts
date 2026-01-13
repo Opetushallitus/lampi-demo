e5f075cb64aa5333d94b8756a91cf076d1689e7a
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2026-01-13 14:12:05 +0200

    LAMPI-358: Create koodisto schema from schema definition file

--- db-task/src/index.ts
+++ db-task/src/index.ts
@@ -1,18 +1,55 @@
 import { text } from 'node:stream/consumers'
+import { Client } from 'pg'
 import { getObjectBody } from './s3'
 
 async function main() {
 	console.info('Starting to restore Koodisto CSV data to database')
 
-	const conf = getConfig()
 	const manifest = await getManifestFile()
+	const schema = await getSchemaFile(manifest.schema.key)
 
-	console.log(JSON.stringify({ manifest, conf }))
+	await withDatabaseTransaction(async (db) => {
+		await createDatabaseWithSchema(db, schema)
+	})
+
+	console.log(JSON.stringify({ manifest }))
+}
+
+function getDatabaseClient() {
+	const { db } = getConfig()
+	return new Client(db)
+}
+
+async function withDatabaseTransaction(
+	functionWithDb: (db: Client) => Promise<void>,
+) {
+	const db = getDatabaseClient()
+	try {
+		await db.connect()
+		await db.query('BEGIN;')
+		await functionWithDb(db)
+		await db.query('COMMIT;')
+	} catch (e) {
+		console.error(`DB transaction failure`, e)
+		await db.query('ROLLBACK;')
+		throw e
+	} finally {
+		await db.end()
+	}
+}
+
+async function createDatabaseWithSchema(db: Client, schema: string) {
+	console.info('Creating database koodisto')
+
+	await db.query(`DROP SCHEMA IF EXISTS koodisto;`)
+	await db.query(`CREATE SCHEMA koodisto;`)
+	await db.query(schema)
+
+	console.info('Database koodisto has been created')
 }
 
 type S3File = {
 	key: string
-	s3Version: string
 }
 
 export type ManifestFile = {
@@ -22,18 +59,21 @@ export type ManifestFile = {
 
 async function getManifestFile(): Promise<ManifestFile> {
 	const manifestBody = await getObjectBody('oph-lampi-local', 'manifest.json')
+	return JSON.parse(await text(manifestBody.Body))
+}
 
-	const manifest = await text(manifestBody.Body)
-	return JSON.parse(manifest)
+async function getSchemaFile(Key: string): Promise<string> {
+	const schema = await getObjectBody('oph-lampi-local', Key)
+	return await text(schema.Body)
 }
 
 function getConfig() {
 	return {
 		db: {
-			hostname: requireEnv('PG_HOST'),
-			port: requireEnv('PG_PORT'),
-			databaseName: requireEnv('DB_NAME'),
-			username: requireEnv('POSTGRES_USER'),
+			host: requireEnv('PG_HOST'),
+			port: parseInt(requireEnv('PG_PORT'), 10),
+			database: requireEnv('DB_NAME'),
+			user: requireEnv('POSTGRES_USER'),
 			password: requireEnv('POSTGRES_PASSWORD'),
 		},
 	}
@@ -55,7 +95,7 @@ main()
 	.catch((e) => {
 		const { db } = getConfig()
 		console.error(
-			`Failed to restore Koodisto data to database ${db.hostname}:${db.port}`,
+			`Failed to restore Koodisto data to database ${db.host}:${db.port}`,
 			e,
 		)
 		process.exit(1)
